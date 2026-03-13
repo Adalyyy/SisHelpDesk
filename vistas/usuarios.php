45246263e8b5782d908220031189e3b80f59806a
Fatima Adaly Sanchez Castro <fatimaadalys@gmail.com>
2026-03-12 19:59:14 -0600

    Correcion de dependencias

--- vistas/usuarios.php
+++ vistas/usuarios.php
@@ -6,12 +6,20 @@
 <div class="container">
   <div class="card border-0 shadow my-5">
     <div class="card-body p-5">
-      <h1 class="fw-light">Usuarios</h1>
-      <p class="lead">Contenido!</p>   
+      <h1 class="fw-light">Administrar Usuarios</h1>
+      <p class="lead">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuarios">
+          Agregar Usuario
+        </button>
+        <hr>
+        <div id="tablaUsuariosLoad"></div>
+      </p>   
   </div>
 </div>
 
-<?php include_once 'footer.php';
+<?php
+ include_once 'usuarios/modalAgregar.php';
+ include_once 'footer.php';
    }else{
       header("location:../index.html");
    }
---
+++ vistas/usuarios/modalAgregar.php
@@ -0,0 +1,73 @@
+
+<!-- Modal -->
+<div class="modal fade" id="modalAgregarUsuarios" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Agregar nuevo usuario</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <div class="row">
+                    <div class="col-sm-4">
+                        <label for="paterno">Apellido Paterno</label>
+                        <input type="text" class="form-control" id="paterno" name="paterno">
+                    </div>
+
+                    <div class="col-sm-4">
+                        <label for="materno">Apellido Materno</label>
+                        <input type="text" class="form-control" id="materno" name="materno">
+                    </div>
+                        
+                    <div class="col-sm-4">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre">
+                    </div>      
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-4">
+                        <label for="paterno">Apellido Paterno</label>
+                        <input type="text" class="form-control" id="paterno" name="paterno">
+                    </div>
+
+                    <div class="col-sm-4">
+                        <label for="materno">Apellido Materno</label>
+                        <input type="text" class="form-control" id="materno" name="materno">
+                    </div>
+                        
+                    <div class="col-sm-4">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre">
+                    </div>      
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-4">
+                        <label for="paterno">Apellido Paterno</label>
+                        <input type="text" class="form-control" id="paterno" name="paterno">
+                    </div>
+
+                    <div class="col-sm-4">
+                        <label for="materno">Apellido Materno</label>
+                        <input type="text" class="form-control" id="materno" name="materno">
+                    </div>
+                        
+                    <div class="col-sm-4">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre">
+                    </div>      
+                </div>
+            
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
