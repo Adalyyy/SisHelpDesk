25c8b58b8c79acdbf2b33685c058e68cf9be03c7
adalyyy <fatimaadalys@gmail.com>
2026-03-17 15:56:04 -0600

    Ya esta validado la columna de activoen el archivo usuario, clases, falta el boton que lo cambie

--- clases/Usuarios.php
+++ clases/Usuarios.php
@@ -7,13 +7,19 @@
             $sql = "SELECT * FROM t_usuarios 
                     WHERE usuario = '$usuario' AND password ='$password'";
             $respuesta= mysqli_query($conexion, $sql);
-            
-            if (mysqli_num_rows($respuesta)>0){
+ 
+            if (mysqli_num_rows($respuesta)>0 ){
                 $datosUsuario= mysqli_fetch_array($respuesta);
-                $_SESSION['usuario']['nombre'] = $datosUsuario['usuario'];
-                $_SESSION['usuario']['id'] = $datosUsuario['id_usuario'];
-                $_SESSION['usuario']['rol'] = $datosUsuario['id_rol'];
+                if($datosUsuario['activo']==1){
+                    $_SESSION['usuario']['nombre'] = $datosUsuario['usuario'];
+                    $_SESSION['usuario']['id'] = $datosUsuario['id_usuario'];
+                    $_SESSION['usuario']['rol'] = $datosUsuario['id_rol'];
                 return 1;
+
+                }else{
+                    return 0;
+                }
+                
             }else{
                 return 0;
             }
